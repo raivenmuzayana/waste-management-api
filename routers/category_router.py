9a5b677c75f373ab5ab5eb8d0971d3f8a8c67adb
6162101117-Andrew <6162101117@student.unpar.ac.id>
2025-12-14 22:04:58 +0700

    hapus create category

--- routers/category_router.py
+++ routers/category_router.py
@@ -11,15 +11,6 @@ router = APIRouter(
     dependencies=[Depends(auth_service.get_current_user)]
 )
 
-# Perhatikan: response_model sekarang pakai WasteCategoryResponse
-@router.post("/", response_model=category_model.WasteCategoryResponse, status_code=status.HTTP_201_CREATED)
-def create_new_category(
-    category: category_model.WasteCategoryCreate, 
-    db: Session = Depends(get_db),
-    current_admin: user_model.User = Depends(auth_service.get_current_admin_user)
-):
-    return category_service.create_category(db=db, category=category)
-
 @router.get("/", response_model=List[category_model.WasteCategoryResponse])
 def read_categories(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     categories = category_service.get_categories(db, skip=skip, limit=limit)
